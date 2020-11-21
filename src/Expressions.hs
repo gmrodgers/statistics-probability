42efc2ec1ebc3c8f4c9b4cc637239274948a69ad
Glen Rodgers <glenmrodgers@gmail.com>
2020-11-21 13:23:31 +0000

    Readdin in x^1 simplify case

--- src/Expressions.hs
+++ src/Expressions.hs
@@ -60,15 +60,21 @@ simplify (Mult (Val 1) y) = simplify y -- 1 * y = y
 simplify (Mult x (Val 1)) = simplify x -- x * 1 = x
 simplify (Mult (Val 0) _) = Val 0 -- 0 * x = 0
 simplify (Mult _ (Val 0)) = Val 0 -- x * 0 = 0
-simplify (Mult (Pow x (Val a)) (Pow y (Val b))) = if x == y then Pow (simplify x) (Val (a + b)) else Mult (Pow (simplify x) (Val a)) (Pow (simplify y) (Val b)) -- x^1 * x^2 = x^3
-simplify (Mult x (Pow y z)) = if x == y then Pow (simplify x) (Add (Val 1) z) else Mult x (Pow (simplify y) (simplify z))
+simplify (Mult (Pow x (Val a)) (Pow y (Val b)))
+  | x == y = Pow (simplify x) (Val (a + b)) -- x^1 * x^2 = x^3
+  | otherwise = Mult (Pow (simplify x) (Val a)) (Pow (simplify y) (Val b))
+simplify (Mult x (Pow y (Val a)))
+  | x == y = Pow (simplify x) (Val (a + 1))
+  | otherwise = Mult (simplify x) (simplify (Pow y (Val a)))
 simplify (Mult (Div (Val 1) x) y) = Div (simplify y) (simplify x)
 simplify (Mult y (Div (Val 1) x)) = Div (simplify y) (simplify x)
 simplify (Mult x y) = Mult (simplify x) (simplify y) -- Simplify down the tree
 simplify (Div (Val a) (Val b)) = Val (a / b) -- 2 / 1 = 2
 simplify (Div (Val 0) _) = Val 0 -- 0 / x = 0
 simplify (Div x (Val 1)) = simplify x -- x / 1 = x
-simplify (Div (Pow x (Val a)) (Pow y (Val b))) = if x == y then Pow (simplify x) (Val (a - b)) else Div (Pow (simplify x) (Val a)) (Pow (simplify y) (Val b)) -- x^2 / x^1 = x^1
+simplify (Div (Pow x (Val a)) (Pow y (Val b)))
+  | x == y = Pow (simplify x) (Val (a - b)) -- x^2 / x^1 = x^1
+  | otherwise = Div (Pow (simplify x) (Val a)) (Pow (simplify y) (Val b))
 simplify (Div x y) = if x == y then Val 1 else Div (simplify x) (simplify y)
 simplify (Pow x (Val 1)) = simplify x -- x^1 = x
 simplify (Pow _ (Val 0)) = Val 1 -- x^0 = 1
