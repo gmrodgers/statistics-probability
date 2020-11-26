0108289f41f6f60f2713e5e0852750647d2786b7
Glen Rodgers <glenmrodgers@gmail.com>
2020-11-26 15:36:31 +0000

    Fix exponential tests to be more accurate -> more reliable

--- test/ContinuousDistributionSpec.hs
+++ test/ContinuousDistributionSpec.hs
@@ -22,25 +22,23 @@ prop_integratesToOneOverFullDomain err n =
 
 prop_integratesToOneOverFullDomainViaNumericalMethod :: Float -> Float -> Bool
 prop_integratesToOneOverFullDomainViaNumericalMethod err n =
-  let res = numericalCDF (Exp n) 0 128 100
+  let res = numericalCDF (Exp n) 0 128 150000
    in res < 1.0 + err && res > 1.0 - err
 
 expSpec :: Spec
 expSpec =
-  let err = 0.01
-      withinErrorOf answer x = (&&) (x < (answer + err)) (x > (answer - err))
+  let withinErrorOf answer err x = (&&) (x < (answer + err)) (x > (answer - err))
    in describe "exp" $ do
         describe "cdf" $ do
           it "integrates to 1 as x -> infinity" $
-            quickCheck $ forAll genPos (prop_integratesToOneOverFullDomain err)
+            property $ forAll genPos (prop_integratesToOneOverFullDomain 0.01)
           it "integrates to correct probability " $
-            cdf (Exp 1) 0 1 `shouldSatisfy` withinErrorOf 0.63212
+            cdf (Exp 1) 0 1 `shouldSatisfy` withinErrorOf 0.01 0.63212
         describe "numerical cdf" $ do
           it "integrates to 1 as x -> infinity" $
-            quickCheck $ forAll genPos (prop_integratesToOneOverFullDomainViaNumericalMethod err)
+            property $ forAll genPos (prop_integratesToOneOverFullDomainViaNumericalMethod 0.05)
           it "integrates to correct probability " $
-            numericalCDF (Exp 1) 0 1 1000 `shouldSatisfy` withinErrorOf 0.63212
-
+            numericalCDF (Exp 1) 0 1 1000 `shouldSatisfy` withinErrorOf 0.05 0.63212
         xdescribe "statistics" $ do
           it "has correct mean" $
             let equivalent = Custom (exponentialPDF 1.0) 0 128
